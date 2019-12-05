d1999dcf51380ef13d6ac3ef81bb79ea1d3ac2b8
Takumi Yoshinaga <takumi.yoshinaga@theport.jp>
2019-12-05 18:05:17 +0900

    delete chrome.tabs.getSelected

--- js/background.js
+++ js/background.js
@@ -1,8 +1,6 @@
 const hatebu = 'https://b.hatena.ne.jp/entry/s/';
 
-chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.getSelected(null, function(tab) {
-        let link = tab.url.replace(/https?:\/\//g, '');
-        chrome.tabs.create({ "url": hatebu + link });
-    });
+chrome.browserAction.onClicked.addListener(function(tab) {
+    let link = tab.url.replace(/https?:\/\//g, '');
+    chrome.tabs.create({ "url": hatebu + link });
 });
